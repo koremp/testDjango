9ce4204990aa0f172667fc5b501098e0bbe26fa0
koremp <im.koremp@gmail.com>
2020-04-06 21:48:43 +0900

    update models.py

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Question(models.Model):
-    question_text = models.CharField(max_lengt=200)
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
