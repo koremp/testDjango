fafd7df94abcd618e80fb0c63db0f852893a7782
koremp <im.koremp@gmail.com>
2020-04-09 22:46:12 +0900

    changed urls.py

--- testDjango/urls.py
+++ testDjango/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('polls/', views.index, name='index'),
     path('polls/<int:question_id>/', views.detail, name='detail'),
     path('polls/<int:question_id>/results/', views.results, name='results'),
-    path('polls/<int:question_id>/vote/', views.vote, name='voteW'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
