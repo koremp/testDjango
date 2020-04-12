877893cadd2c0b9550620612dda9a4b0406e2b05
koremp <im.koremp@gmail.com>
2020-04-13 00:03:35 +0900

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
+# 필드 순서 변경
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('None', {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
+
+inlines = [ChoiceInline]
+list_display = {'question_text', 'pub_date'}
